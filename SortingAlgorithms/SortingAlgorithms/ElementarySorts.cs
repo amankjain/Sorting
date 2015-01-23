4686b99f729dab201e15877889b50f8807b27311
Aman Kumar Jain <amankjain@gmail.com>
2015-01-22 18:19:11 -0800

    xv

--- SortingAlgorithms/SortingAlgorithms/ElementarySorts.cs
+++ SortingAlgorithms/SortingAlgorithms/ElementarySorts.cs
@@ -8,13 +8,28 @@ namespace SortingAlgorithms
 {
     public class ElementarySorts
     {
+        public void swap(long[] array, int positionX, int positionY)
+        {
+            long value = array[positionX];
+            array[positionX] = array[positionY];
+            array[positionY] = value;
+        }
         public long[] bubbleSort(long[] array)
         {
-            for (int x = 0; x < array.Count<long>(); x++ )
+            for (int x = 0; x < array.Length; x++ )
             {
-
+                for (int y = x+1; y < array.Length; y++)
+                {
+                    if (y <= (array.Length - 1))
+                    {
+                        if (array[y] < array[x])
+                        {
+                            swap(array, x, y);
+                        }
+                    }
+                }
             }
-                return array;
+            return array;
         }
     }
 }
--- SortingAlgorithms/TestProject/Program.cs
+++ SortingAlgorithms/TestProject/Program.cs
@@ -4,12 +4,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using SortingAlgorithms;
+
 namespace TestProject
 {
     class Program
     {
+        public static void printArray(long[] array)
+        {
+            for(int x=0; x < array.Length; x++)
+            {
+                Console.Write(array[x].ToString() + '\t');
+            }
+            Console.Write('\n');
+        }
         static void Main(string[] args)
         {
+            ElementarySorts e1 = new ElementarySorts();            
+            long[] arrayUnsorted = { 100, 98, 1, 55, 20, 33, 5 };
+            printArray(arrayUnsorted);
+            long[] arraySorted = e1.bubbleSort(arrayUnsorted);
+            printArray(arraySorted);
+            Console.ReadLine();
         }
     }
 }
