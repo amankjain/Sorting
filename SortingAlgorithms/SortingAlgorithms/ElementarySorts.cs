614ccf13c8260fdf9cab9e81e0cb32ea9e44cfbd
Aman Kumar Jain <amankjain@gmail.com>
2015-01-22 18:35:50 -0800

    cv

--- SortingAlgorithms/SortingAlgorithms/ElementarySorts.cs
+++ SortingAlgorithms/SortingAlgorithms/ElementarySorts.cs
@@ -14,8 +14,46 @@ namespace SortingAlgorithms
             array[positionX] = array[positionY];
             array[positionY] = value;
         }
+        public long[] SelectionSort(long[] array)
+        {
+            return array;
+        }
         public long[] bubbleSort(long[] array)
         {
+            //optimzation: Check if swaps are made in the iteration, no swaps then exit, because it means its already sorted
+            /*C++
+             bool swapped = true;
+
+                  int j = 0;
+
+                  int tmp;
+
+                  while (swapped) {
+
+                        swapped = false;
+
+                        j++;
+
+                        for (int i = 0; i < n - j; i++) {
+
+                              if (arr[i] > arr[i + 1]) {
+
+                                    tmp = arr[i];
+
+                                    arr[i] = arr[i + 1];
+
+                                    arr[i + 1] = tmp;
+
+                                    swapped = true;
+
+                              }
+
+                        }
+
+                  }
+             * */
+
+            //N^2 
             for (int x = 0; x < array.Length; x++ )
             {
                 for (int y = x+1; y < array.Length; y++)
