7b60b8777d1d1d6714dcffc29817756abfe57003
Aman Kumar Jain <amankjain@gmail.com>
2015-01-27 18:12:10 -0800

    0127

--- SortingAlgorithms/SortingAlgorithms/MergeSort.cs
+++ SortingAlgorithms/SortingAlgorithms/MergeSort.cs
@@ -8,5 +8,40 @@ namespace SortingAlgorithms
 {
     class MergeSort
     {
+        //Stable (No swaps needed) -mergesort uses between 1/2 N lg N and N lg N compares and at most 6 N lg N array accesses to sort any array of length N. 
+        //Mergesort is an asymptotically optimal compare-based sorting algorithm. 
+        //That is, both the number of compares used by mergesort in the worst case and the minimum number of compares that any compare-based sorting algorithm can guarantee are ~N lg N. 
+        //Only CON is that extra space propotional to N
+
+        public IComparable[] sort(IComparable[] array, IComparable[] auxArray)
+        {
+            sort(array, auxArray, low, mid);
+            return array;
+        }
+        private void sort(IComparable[] arrayToSort, IComparable[] auxArray, int low, int high)
+        {
+            
+        }
+        private IComparable[] merge(IComparable[] arrayLow, IComparable[] arrayHigh, IComparable[] auxArray, int low, int high, int mid)
+        {
+
+        }
+
+        public bool isSorted(IComparable [] array)
+        {
+            for(int x=0; x < array.Length; x++)
+            {
+                if(less(array[x],array[x+1]) == false)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        public bool less(IComparable a, IComparable b)
+        {
+            return a.CompareTo(b) < 0; //<0 is less thab ;0 is same; >0 is greater than
+        }
+
     }
 }
