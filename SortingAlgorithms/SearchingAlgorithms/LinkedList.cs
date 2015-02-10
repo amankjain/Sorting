1c749872fbd351a9ca95f2bbef66ccb0fe001453
Aman Kumar Jain <amankjain@gmail.com>
2015-02-09 22:24:14 -0800

    hkj

---
+++ SortingAlgorithms/SearchingAlgorithms/LinkedList.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchingAlgorithms
+{
+    public class Node<T> where T: IComparable
+    {
+        public string key;
+        public T value;
+    }
+    public class LinkedList<T> where T: IComparable
+    {
+        Node<T> head;
+        Node<T> next;
+    }
+}
--- bitWise/bitWise/Program.cs
+++ bitWise/bitWise/Program.cs
@@ -8,8 +8,45 @@ namespace bitWise
 {
     class Program
     {
+        public static uint swapByteOrder(uint value)
+        {
+            //http://stackoverflow.com/questions/5320439/how-do-i-swap-endian-ness-byte-order-of-a-variable-in-javascript
+
+            //text file in project has content of above link
+            return value;
+
+        }
         static void Main(string[] args)
         {
+              /*
+             * Set union
+A | B
+Set intersection
+A & B
+Set subtraction
+A & ~B
+Set negation
+ALL_BITS ^ A
+Set bit
+A |= 1 << bit
+Clear bit
+A &= ~(1 << bit)
+Test bit
+(A & 1 << bit) != 0
+             * */
+            //Test A bit
+      //      int A = 55;
+        //    bool 4thBitValueIsFalse = ((A & (1 << 4)) == 0);
+
+          
+
+            //big endian to little endian
+      /*      i = (data[0]<<0) | (data[1]<<8) | (data[2]<<16) | (data[3]<<24);
+If it's big-endian, here's how to extract it:
+i = (data[3]<<0) | (data[2]<<8) | (data[1]<<16) | (data[0]<<24);
+            */
+
+
         }
     }
 }
